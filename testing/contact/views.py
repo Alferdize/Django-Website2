83320185f5ff3ac1972bb7e4b1e0d5a59ff2586c
Alferdize <33134277+Alferdize@users.noreply.github.com>
2018-05-30 01:12:49 +0530

    first commit

--- testing/contact/views.py
+++ testing/contact/views.py
@@ -8,16 +8,21 @@ from django.core.mail import send_mail
 
 
 def contact(request):
+    title = 'Contact'
     form = contactForm(request.POST or None)
-
+    confirm_message = None
     if form.is_valid():
         name = form.cleaned_data['name']
         comment = form.cleaned_data['comment']
         subject = 'MESsage from mysite.com'
-        message = comment,name
+        message = '%s %s' %(comment,name)
         emailFrom = form.cleaned_data['email']
         emailTo = [settings.EMAIL_HOST_USER]
         send_mail(subject, message, emailFrom, emailTo, fail_silently=True)
-    context = locals()
+        title = "Thanks!"
+        confirm_message = "Thanks for the message. We will get right back to you."
+        form = None
+
+    context = {'title': title, 'form': form, 'confirm_message' : confirm_message, }
     template = 'contact.html'
     return render(request,template,context)
--- testing/testing/urls.py
+++ testing/testing/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
 
 from profiles import views as profile_views
 from contact import views as contact_views
@@ -28,8 +28,36 @@ urlpatterns = [
     path('', profile_views.home, name='home'),
     path('about/', profile_views.about, name='about'),
     path('contact/',contact_views.contact, name='contact'),
+    path('accounts/', include('allauth.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+"""
+	path("signup/", profile_views.signup, name="account_signup"),
+    path("login/", profile_views.login, name="account_login"),
+    path("logout/", profile_views.logout, name="account_logout"),
+    path("password/change/", views.password_change,
+        name="account_change_password"),
+    path("password/set/", views.password_set, name="account_set_password"),
+
+    path("inactive/", views.account_inactive, name="account_inactive"),
+
+    # E-mail
+    path("email/", views.email, name="account_email"),
+    path("confirm-email/", views.email_verification_sent,
+        name="account_email_verification_sent"),
+    re_path("confirm-email/(?P<key>[-:\w]+)/", views.confirm_email,
+        name="account_confirm_email"),
+
+    # password reset
+    path("password/reset/", views.password_reset,
+        name="account_reset_password"),
+    path("password/reset/done/", views.password_reset_done,
+        name="account_reset_password_done"),
+    re_path("password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/",
+        views.password_reset_from_key,
+        name="account_reset_password_from_key"),
+    path("password/reset/key/done/", views.password_reset_from_key_done,
+        name="account_reset_password_from_key_done"),"""
